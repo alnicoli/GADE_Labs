7f2313a74e44e980fbbf0bde4a6b3e115446f21f
Alessandro Nicoli <chratzchopf@hotmail.com>
2018-04-17 12:45:30 +0200

    Added Lab8

---
+++ GADE_Lab8/Assets/Imports/wing_mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wing_mover : MonoBehaviour {
+
+	bool wingsExtended = true;
+	GameObject leftWing;
+	GameObject rightWing;
+	Vector3 positionOfDog;
+
+	// Use this for initialization
+	void Start () {
+		leftWing = this.transform.Find ("LeftWing").gameObject;
+		rightWing = this.transform.Find ("RightWing").gameObject;
+		positionOfDog = this.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		positionOfDog = this.transform.position;
+		if (Input.GetKeyDown("space")) {
+			if (wingsExtended) {
+				leftWing.transform.RotateAround (positionOfDog, Vector3.forward, -90);
+				rightWing.transform.RotateAround (positionOfDog, Vector3.forward, +90);
+				wingsExtended = false;
+			} else {
+				leftWing.transform.RotateAround (positionOfDog, Vector3.forward, 90);
+				rightWing.transform.RotateAround (positionOfDog, Vector3.forward, -90);
+				wingsExtended = true;
+			}
+		}
+	}
+
+}
